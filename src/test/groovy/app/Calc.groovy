bd04e9f992c3a288ba065def4f3127ede2d122cd
terahide <terahide27@gmail.com>
2019-06-06 17:56:42 +0900

    sitimiをメインに移動

--- src/test/groovy/app/Calc.groovy
+++ src/test/groovy/app/Calc.groovy
@@ -4,7 +4,7 @@ import sitimi.Application
 
 class Calc extends Application{
     def startOperation(){
-        click 'imgs/windows_start.png'
+        click 'src/test/imgs/windows_start.png'
         write 'calc#ENTER.'
     }
 }
--- src/test/groovy/app/CalcWindow.groovy
+++ src/test/groovy/app/CalcWindow.groovy
@@ -5,15 +5,15 @@ import sitimi.Window
 class CalcWindow extends Window{
     // def at(){ title == '電卓'}
     def "足し算する"(i, j){
-        click "imgs/${i}_button.png"
-        click "imgs/plus_button.png"
-        click "imgs/${j}_button.png"
-        click "imgs/equal_button.png"
+        click "src/test/imgs/${i}_button.png"
+        click "src/test/imgs/plus_button.png"
+        click "src/test/imgs/${j}_button.png"
+        click "src/test/imgs/equal_button.png"
     }
     def "結果は"(r){
-        exists "imgs/${r}.png"
+        exists "src/test/imgs/${r}.png"
     }
     def "ウィンドウを閉じる"(){
-        click 'imgs/window_close.png'
+        click 'src/test/imgs/window_close.png'
     }
 }
